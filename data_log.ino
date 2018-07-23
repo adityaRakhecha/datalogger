b51e05957c5d568d522932abebdb4f58726bb6a0
Anirudh Swarankar <swarnakar.ani24@gmail.com>
2018-07-23 15:21:22 +0530

    update gps

--- data_log.ino
+++ data_log.ino
@@ -4,6 +4,7 @@
 #include <SD.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
+#include <math.h>
 
 DS1307 rtc;
 RTCDateTime dt;
@@ -30,13 +31,13 @@ static void print_str(const char *str, int len);
 float flat, flon;
 unsigned long age, date, time, chars = 0;
 unsigned short sentences = 0, failed = 0;
-static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
-
+char sz[32];
+String temp;
 
 void setup() {
   rtc.begin();
   SD.begin(CS);
-  ss.begin(9600); //if 9600 doesnt work change it to 4800
+  ss.begin(4800); //if 9600 doesnt work change it to 4800
   //if date not set 
   if(!rtc.isReady()){
     rtc.setDateTime(__DATE__,__TIME__);
@@ -56,8 +57,20 @@ if(dataFile){
 }
 
 void loop() {
+  dataString="";
   // put your main code here, to run repeatedly:
 dt=rtc.getDateTime();
+ print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
+dataString=temp+" ";
+gps.f_get_position(&flat, &flon, &age);
+print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
+dataString=dataString+temp+" ";
+print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
+dataString=dataString+temp+" ";
+print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+dataString=dataString+temp+" ";
+print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
+dataString=dataString+temp+" ";
 
 
 
@@ -66,4 +79,46 @@ dt=rtc.getDateTime();
 
 
 
+}
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
+
+static void print_int(unsigned long val, unsigned long invalid, int len)
+{
+   if (val == invalid)
+    strcpy(sz, "*******");
+  else
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  temp=String(sz);
+  smartdelay(0);
+}
+static void print_float(float val, float invalid, int len, int prec)
+{
+  if (val == invalid)
+  {
+    while (len-- > 1)
+      sprintf(temp,"***");
+      }
+  else
+  {
+    sprintf(temp,"%0.6f",val);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+    temp=temp+" ";
+  }
+  smartdelay(0);
 }
